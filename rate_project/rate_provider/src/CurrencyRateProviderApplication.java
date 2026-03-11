1a5d1ec7bb096cecb61b6b380776590cd3ebc941
Perunova Mariia <94389641+loc43@users.noreply.github.com>
2026-03-11 20:20:26 +0300

    Update CurrencyRateProviderApplication.java

--- rate_project/rate_provider/src/CurrencyRateProviderApplication.java
+++ rate_project/rate_provider/src/CurrencyRateProviderApplication.java
@@ -1,1 +1,11 @@
+package com.example.currencyrateprovider;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyRateProviderApplication.class, args);
+    }
+}
